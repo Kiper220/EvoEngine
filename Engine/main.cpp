58db4f6f08baa4880ccd7fb80ca6f607d3771035
Kiper220 <gkya@wetgrape.su>
2020-06-21 23:04:55 +0400

    Refactor main.cpp

--- Engine/main.cpp
+++ Engine/main.cpp
@@ -7,7 +7,7 @@ using namespace RTL;
 
 
 int main(){
-    Directory dir("./Mods");
+    Directory dir(".");
     std::cout << "All Objects Count: " << EntitiesCount(dir)
     << "\nAll File Count: " << FileCount(dir)
     << "\nAll Directories Count: " << DirectoriesCount(dir);
